7b68c86daf5ebf89e32268e7a1d452b56f62086f
Kasper Plougmann <GitHub@squazz.dk>
2015-02-12 13:54:06 +0100

    Added missing files

---
+++ Squazz.HotCiv.Core.Tests/GammaCivTests.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Squazz.HotCiv.Strategies;
+
+namespace Squazz.HotCiv.Core.Tests
+{
+    [TestClass]
+    public class GammaCivTests
+    {
+        Game _game;
+        [TestInitialize]
+        public void Initialize()
+        {
+            _game = new Game(new AgeStrategyLinear(), new WinningStrategyRedWins(), new ActionStrategyDoAction());
+        }
+
+        [TestMethod]
+        public void ShouldBeAbleToPerformArcherAction()
+        {
+            Position archerPosition = new Position(2,0);
+            _game.PerformUnitActionAt(archerPosition);
+            Assert.AreEqual(6, _game.GetUnitAt(archerPosition).Defense);
+            Assert.AreEqual(0, _game.GetUnitAt(archerPosition).Moves);
+            EndRounds();
+            Assert.AreEqual(6, _game.GetUnitAt(archerPosition).Defense);
+            Assert.AreEqual(0, _game.GetUnitAt(archerPosition).Moves);
+        }
+
+        [TestMethod]
+        public void PerformingArcherAction2TimesShouldResetTheFortification()
+        {
+            Position archerPosition = new Position(2, 0);
+            _game.PerformUnitActionAt(archerPosition);
+            Assert.AreEqual(6, _game.GetUnitAt(archerPosition).Defense);
+            Assert.AreEqual(0, _game.GetUnitAt(archerPosition).Moves);
+            EndRounds();
+            Assert.AreEqual(6, _game.GetUnitAt(archerPosition).Defense);
+            Assert.AreEqual(0, _game.GetUnitAt(archerPosition).Moves);
+            _game.PerformUnitActionAt(archerPosition);
+            Assert.AreEqual(3, _game.GetUnitAt(archerPosition).Defense);
+            Assert.AreEqual(0, _game.GetUnitAt(archerPosition).Moves);
+            EndRounds();
+            Assert.AreEqual(3, _game.GetUnitAt(archerPosition).Defense);
+            Assert.AreEqual(1, _game.GetUnitAt(archerPosition).Moves);
+        }
+
+        [TestMethod]
+        public void ShouldBeAbleToPerformSettlerAction()
+        {
+            Position settlerPosition = new Position(4, 3);
+            Assert.IsNotNull(_game.GetUnitAt(settlerPosition));
+            _game.PerformUnitActionAt(settlerPosition);
+            Assert.IsNull(_game.GetUnitAt(settlerPosition));
+            Assert.IsNotNull(_game.GetCityAt(settlerPosition));
+            Assert.AreEqual(Player.RED, _game.GetCityAt(settlerPosition).Owner);
+        }
+
+        // Shortcode for ending 2 turns
+        private void EndRounds(int rounds = 1)
+        {
+            for (int i = 1; i <= rounds; i++)
+            {
+                _game.EndOfTurn();
+                _game.EndOfTurn();
+            }
+        }
+    }
+}
---
+++ Squazz.HotCiv.Core/Strategies/ActionStrategyDoAction.cs
@@ -0,0 +1,23 @@
+﻿namespace Squazz.HotCiv.Strategies
+{
+    public class ActionStrategyDoAction : IActionStrategy
+    {
+        public bool PerformAction(Position position, IGame game)
+        {
+            IUnit unit = game.GetUnitAt(position);
+            switch (unit.Type)
+            {
+                case GameConstants.Archer:
+                    if (unit.Defense == 6)
+                        unit.Defense = 3;
+                    else
+                        unit.Defense = unit.Defense * 2;
+                        unit.Moves = 0;
+                    return true;
+                case GameConstants.Settler:
+                    return true;
+            }
+            return false;
+        }
+    }
+}
