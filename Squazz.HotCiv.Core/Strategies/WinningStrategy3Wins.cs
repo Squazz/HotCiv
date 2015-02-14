d3131667192be72030f95ee97d35467bdb83979e
Kasper Plougmann <GitHub@squazz.dk>
2015-02-14 12:48:17 +0100

    Added needed files for upcoming excercises

---
+++ Squazz.HotCiv.Core/Strategies/WinningStrategy3Wins.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Squazz.HotCiv.Strategies
+{
+    class WinningStrategy3Wins : IWinningStrategy
+    {
+        public Player? GetWinner(int age, Dictionary<Position, ICity> cities)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
