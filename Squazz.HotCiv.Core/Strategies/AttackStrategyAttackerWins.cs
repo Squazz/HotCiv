fd1dd3da9dcc34fd1460a1d08b1b1919a2eb10cb
Kasper Plougmann <GitHub@squazz.dk>
2015-02-14 19:42:54 +0100

    Added missing files

---
+++ Squazz.HotCiv.Core/Strategies/AttackStrategyAttackerWins.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Squazz.HotCiv.Strategies
+{
+    public class AttackStrategyAttackerWins : IAttackStrategy
+    {
+        public IUnit Attack(Position attacker, Position defence, 
+            Dictionary<Position, ICity> cities, Dictionary<Position, IUnit> units, 
+            Dictionary<Position, ITile> tiles = null,  Dictionary<Position, IUnit> fortifiedArchers = null)
+        {
+            IUnit unit;
+            var result = units.TryGetValue(attacker, out unit);
+
+            if (result)
+                units.Remove(defence);
+            return unit;
+        }
+    }
+}
